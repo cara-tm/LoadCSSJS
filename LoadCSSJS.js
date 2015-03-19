e771bfbb2e892b3782c62c0169e940a6fcc79984
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2015-03-19 10:12:02 +0100

    Update LoadCSSJS.js
    Rewrite code. Avoid double file loading into HTML document.
    Tested with Google Page Speed.

--- LoadCSSJS.js
+++ LoadCSSJS.js
@@ -2,30 +2,23 @@ function LoadCSSJS(e, t, m){
 
 	"use strict";
 
-	var ref = window.document.getElementsByTagName("script")[0];
-
-	if (t == 'css') {
-		var f=document.createElement('link');
+	if (t == "css") {
+		var f=document.createElement("link");
 		f.setAttribute("rel", "stylesheet");
-    f.setAttribute("href", e);
-    (m ? '' : m='all');
-    f.setAttribute("media", m);
-    f.setAttribute("type", "text/css");
-	} else if (t == 'js') {
-		var f = document.createElement('script');
+    		f.setAttribute("href", e);
+    		(m ? "" : m="all");
+    		f.setAttribute("media", m);
+    		f.setAttribute("type", "text/css");
+	} else if (t == "js") {
+		var f = document.createElement("script");
 		f.setAttribute("type", "text/javascript");
-        f.setAttribute("src", e);
+        	f.setAttribute("src", e);
 	}
 	if (typeof f != "undefined") {
 		document.getElementsByTagName("head")[0].appendChild(f);
-		ref.parentNode.insertBefore(f, ref);
+		var s = document.getElementsByTagName("script");
+		s = s[s.length - 1];
+		s.parentNode.insertBefore(f, s);
+	}
 
-		var sheets = window.document.styleSheets;
-	}
-	function toggles() {
-		for(var e, t=0; t < sheets.length; ++t)
-			sheets[t].e && sheets[t].e.indexOf(href) > -1 && (e =! 0);
-			e || setTimeout(toggles)
-	}
-	toggles();
 }
