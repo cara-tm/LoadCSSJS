325cf869f3d935801bb45bf53ceff96aadaf2124
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2016-01-03 11:04:43 +0100

    Update LoadCSSJS.js
    Didn't noticed I previously added a Timeout.

--- LoadCSSJS.js
+++ LoadCSSJS.js
@@ -36,12 +36,9 @@ var LoadCSSJS = function ( e, t, o ) {
 	if ( "undefined" != typeof s ) {
 
 		var a = document.getElementsByTagName( "script" )[0];
-		window.setTimeout(function() {
-				t == "css" ? 
-					document.getElementsByTagName("head")[0].appendChild(s) : 
-					a.parentNode.insertBefore(s, a);
-			}, 35);
-			window.clearTimeout(a);
+		t == "css" ? 
+			document.getElementsByTagName("head")[0].appendChild(s) : 
+		a.parentNode.insertBefore(s, a);
 
 	} else {
 
